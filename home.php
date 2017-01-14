43365fab64d30177da989c0d1cbdcca44fec1e1d
Daniel Oraca <daniel.oraca@gmail.com>
2017-01-14 07:28:15 +0000

    save

---
+++ home.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html PUBLIC
+    "-//W3C//DTD HTML 4.01//EN"
+    "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+<title>Casey</title>
+<meta name="description" content="a description of your page"> 
+<meta name="keywords" content="a, list, of, keywords"> 
+<link rel="stylesheet" type="text/css" href="styles.css">
+<script type="text/javascript" src="scripts/external.js"></script>
+</head>
+<body>
+<div id="wrapper">
+	<div id="header"> 
+		<h1><a href="home.php" title="Casey Contracting">Casey Contracting<span>&nbsp;
+		</span></a>
+		</h1>
+
+	<div id="mainMenuContainer">
+		<?php
+		require("mainMenu.html");
+		?>
+	</div>
+	</div>
+
+<div id="container">
+
+	<div id="flashContent">
+
+		<object type="application/x-shockwave-flash" 
+		data="flash/header.swf" width="816" height="263">
+		<param name="movie" value="flash/header.swf" />
+		<param name="wmode" value="transparent" />
+		<!-- no flash content -->
+		<p>
+		Attempting to load flash content. If your system does not have the latest flash player installed 
+		please visit the <a href="http://www.adobe.com/products/flashplayer">Adobe Flash Player Website</a> 
+		to get it.
+		</p>
+		<!-- end no flash content -->
+		</object>
+	</div>
+
+	<div id="content">
+	<img class="imghome" src="homeSideImage.jpg" width="234" height="347" alt="home interior">
+	<h2>ABOUT CASEY CONTRACTING:</h2>
+	<p>After completing high school, and setting my sights to become a pilot, I spent the summer of 1978 
+	working for a local carpenter.  Not only was this occupation fulfilling, it was giving me a sense of 
+	accomplishment at the end of each and every day.  It wasn't long before I realized there might be a need 
+	to re-consider my career...
+	</p>
+	<p>I knew I wanted to work in Muskoka and started a construction company. Casey Contracting was formed.
+	</p>
+	<p>Thirty Years later I am still swinging a hammer, and enjoying the continued fulfillment of working in 
+	one of Ontario's treasured regions.
+	</p>
+	<p>I am very fortunate to be making a living in Muskoka, and continue to be excited about every project 
+	taking place.  The rewards are endless and they all result from a number of criteria, the most important 
+	being... the people we work for.  Without our clients, and potential clients.......where would we be?
+	With all the good fortune of a healthy working atmosphere, working with my hands, a career in Cottage 	
+	Country, and doing what I love to do, there is no way I would jeopardize my situation.
+	</p>
+	<p>Thirty  years, and more to come.</p>
+	<p>That is the basis of a company you can trust.</p>
+
+	<p><strong>Build in Muskoka. Get Hooked on the Feeling</strong></p>
+
+	<p class="cary">- Cary J. Cain</p><br><br><br>
+	</div>
+	<div id="footer">
+		<?php
+		require("footerMenu.html");
+		?>
+		
+		<p>Copyright &copy; 2008, Casey Contracting All Rights Reserved | 
+		<a href="http://www.divinedesigns.ca" rel="external"> 
+		Website Design by <span>Divine Designs.ca</span> - Divinely Inspired Web Design
+		</a>
+		</p>
+	</div>
+
+
+</div>
+</div>
+</body>
+</html>
